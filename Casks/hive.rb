28de3c54b34f058ba3405c8c263598535f681e8f
morapelker <morapelker@gmail.com>
2026-02-12 20:53:58 +0200

    m

--- Casks/hive.rb
+++ Casks/hive.rb
@@ -1,7 +1,7 @@
 cask "hive" do
   version "1.0.0"
   sha256 "3ce0654913d092f824c468af3952dbfb512e7003bd168988e1fcbd5ac37f17df"
-  url "https://github.com/morapelker/hive/releases/download/v#{version}/Hive-#{version}.dmg"
+  url "https://github.com/morapelker/hive/releases/download/v#{version}/v#{version}.dmg"
   name "Hive"
   homepage "https://github.com/morapelker/hive"
   app "Hive.app"
