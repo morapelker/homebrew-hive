50e8e787bb19d2b2de1c36a5192ed95ad16e76c9
morapelker <morapelker@gmail.com>
2026-02-13 12:40:42 +0200

    m

--- Casks/hive.rb
+++ Casks/hive.rb
@@ -1,13 +1,13 @@
 cask "hive" do
-  version "1.0.6"
+  version "1.0.7"
 
   on_arm do
-    sha256 "e9abf154ecc4b1ddba97a9a71faa0b8bd187a58f7c1022246101b2b2837eb4b5"
+    sha256 "af0a9acc7f51941f6a2872244957e9c653e60c0a9fa181adcd815cd904adc55b"
     url "https://github.com/morapelker/hive/releases/download/v#{version}/Hive-#{version}-arm64.dmg"
   end
 
   on_intel do
-    sha256 "7b0f533799d5ab13f93174cb91e2745b9e58895dfa3ebf5cc5e5bf534e3bce1f"
+    sha256 "bcbae72c645111302e8632814e8d422c2cd9270338f5d9728e4634cf02493ffc"
     url "https://github.com/morapelker/hive/releases/download/v#{version}/Hive-#{version}.dmg"
   end
 
