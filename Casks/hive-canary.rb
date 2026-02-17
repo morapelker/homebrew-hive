f3373e13fd6fdf536ce73232e4595c81d202c30f
morapelker <morapelker@gmail.com>
2026-02-17 13:45:23 +0200

    Update Hive Canary to v1.0.19-canary.7

--- Casks/hive-canary.rb
+++ Casks/hive-canary.rb
@@ -1,13 +1,13 @@
 cask "hive-canary" do
-  version "1.0.19-canary.6"
+  version "1.0.19-canary.7"
 
   on_arm do
-    sha256 "4405f777d041355b81dc4a3d7356ffd6949a221adb86312517620be31907c859"
+    sha256 "7f5d7c09e6bb432a21f94e7dddf9da5306457c8ed44edd9c5d66f757920a1843"
     url "https://github.com/morapelker/hive/releases/download/v#{version}/Hive-#{version}-arm64.dmg"
   end
 
   on_intel do
-    sha256 "287f4fa46dd81a17d1af1fabffaf20c3e19e8332caf50fd9ba034097569e9ee3"
+    sha256 "8fbf8cf7b7bbc3c8f3d97dd6994fba789567efe3cb4d4e4e71ec6f6b6b806637"
     url "https://github.com/morapelker/hive/releases/download/v#{version}/Hive-#{version}.dmg"
   end
 
