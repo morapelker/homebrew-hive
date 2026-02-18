3a234bea3b642446d518c763a502529fc66f7fb5
morapelker <morapelker@gmail.com>
2026-02-18 12:11:48 +0200

    Update Hive to v1.0.22

--- Casks/hive.rb
+++ Casks/hive.rb
@@ -1,13 +1,13 @@
 cask "hive" do
-  version "1.0.21"
+  version "1.0.22"
 
   on_arm do
-    sha256 "edb63dc08e1a50ea040c518706e224ca896dd91c3906f21ebb85fd1357c82747"
+    sha256 "1659233f6fb7efbbda47feb1dd9728df5f82edbc0ff695cda8e2b7f6407087b3"
     url "https://github.com/morapelker/hive/releases/download/v#{version}/Hive-#{version}-arm64.dmg"
   end
 
   on_intel do
-    sha256 "bb8683f05bcc057aa269dfd1145ff65fc3418972d3bc4fbebdf2c3e4abd46874"
+    sha256 "004c8a4201e566e3beed5d603228b84237080dab33fdf772ba1f7eb23dfee795"
     url "https://github.com/morapelker/hive/releases/download/v#{version}/Hive-#{version}.dmg"
   end
 
